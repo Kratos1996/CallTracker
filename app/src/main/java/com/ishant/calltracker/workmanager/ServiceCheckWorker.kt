8b2c1f6c1461a0a01dac4e874d9cf1f8ca4951fe
rituraj sharma <rituraj152150@gmail.com>
2024-01-18 15:37:59 +0530

    [Done] call tracker : Multiple receiver part :Done

---
+++ app/src/main/java/com/ishant/calltracker/workmanager/ServiceCheckWorker.kt
@@ -0,0 +1,27 @@
+package com.ishant.calltracker.workmanager
+
+import android.content.Context
+import android.util.Log
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+import com.ishant.calltracker.service.CallService
+import com.ishant.calltracker.service.ServiceRestarterService
+import com.ishant.calltracker.utils.isServiceRunning
+import com.ishant.calltracker.utils.navToCallService
+
+
+class ServiceCheckWorker(val context: Context, params: WorkerParameters) : Worker(context, params) {
+
+    override fun doWork(): Result {
+        if (!context.isServiceRunning(CallService::class.java)) { // Replace with your service class
+            Log.e(ServiceRestarterService.TAG, "CallTracker : Workmanger > ServiceCheckWorker > doWork > CallService is not running. Restarting...")
+            context.navToCallService()
+        }
+        else{
+            Log.e(ServiceRestarterService.TAG, "CallTracker : Workmanger > ServiceCheckWorker > doWork > CallService service is running....")
+        }
+
+        return Result.success()
+    }
+
+}

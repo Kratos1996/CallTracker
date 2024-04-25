675f57bc69206223561f1794c57c172016ecaf2b
ishant sharma <ishant.sharma1947@gmail.com>
2024-04-25 23:21:20 +0530

    [Done] lib updated Done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -151,6 +151,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.composeUiTooling)
     implementation(libs.bundles.org.ishant.compose.libs )
     implementation(libs.lifecycle.viewmodel.compose)
-    implementation ("com.github.Kratos1996:corelib:1.0.9@aar")
+    implementation ("com.github.Kratos1996:corelib:1.1.0@aar")
     implementation(libs.bundles.image.libs)
 }

d9ffeb34c97a9e7fcad45b42c6d4bb9ab3250bf4
Lakshya-2001 <86869227+Lakshya-2001@users.noreply.github.com>
2024-10-13 23:42:54 +0530

    jks and version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.ishant.callsoftware"
         minSdk = 25
         targetSdk = 34
-        versionCode = 17
-        versionName = "2.0.2"
+        versionCode = 1
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
